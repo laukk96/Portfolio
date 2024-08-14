cd30e05c4c70ccc86e7a1bd22821d9d0c45c4681
pluffpenguin <lau.kiaran@gmail.com>
2024-08-14 14:38:33 -0500

    adding white navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import StickyTop from './Components';
 
 function App() {
   return (
     <div className="App">
+      <StickyTop>Nav bar here!</StickyTop>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Components.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function StickyTop() {
+    return (
+        <nav class="stickynav">
+            <ul>
+                <li> list item 1</li>
+                <button>Here</button>
+            </ul>
+        </nav>
+    )
+}
