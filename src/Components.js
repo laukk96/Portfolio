3356999ea4a6c7cd17689b7377a31e0e13c2d7d5
pluffpenguin <lau.kiaran@gmail.com>
2024-08-14 23:49:48 -0500

    fixed horizontal nav bar

--- src/Components.js
+++ src/Components.js
@@ -1,11 +1,15 @@
 import React from "react"
+import "./Components.css"
 
 export default function StickyTop() {
     return (
-        <nav class="stickynav">
+        <nav className ="stickynav">
             <ul>
-                <li> list item 1</li>
-                <button>Here</button>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>
+                    <button>button text</button>
+                </li>
             </ul>
         </nav>
     )
